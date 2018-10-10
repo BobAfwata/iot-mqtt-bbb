f7f511b7e193ca9f57a5461ba334c0550038d86f
Drew Moseley <drew.moseley@northern.tech>
2018-10-10 10:50:57 -0400

    Add python sample apps
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

---
+++ iot-mqtt-bbb-actuator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import paho.mqtt.client as mqtt
+
+def onConnect(client, obj, flags, rc):
+    print("Connected. rc = %s " % rc)
+    client.subscribe("iot-bbb-example/weather/temperature")
+    client.subscribe("iot-bbb-example/weather/precipitation")
+
+# Dummy function to act on temperature data
+def temperatureActuator(temperature):
+    print("Temperature = %s\n" % temperature)
+
+# Dummy function to act on precipitation data
+def precipitationActuator(precipitation):
+    action = {
+        "rain" : "Grab an umbrella",
+        "sun" : "Don't forget the sunscreen",
+        "hurricane" : "Buy bread, water and peanut butter."
+    }
+    print("Precipitation = %s" % precipitation)
+    print("\t%s\n" % action[precipitation])
+    
+def onMessage(mqttc, obj, msg):
+    callbacks = {
+        "iot-bbb-example/weather/temperature" : temperatureActuator,
+        "iot-bbb-example/weather/precipitation" : precipitationActuator
+    }
+    callbacks[msg.topic](msg.payload)
+
+client = mqtt.Client()
+client.on_connect = onConnect
+client.on_message = onMessage
+client.connect("test.mosquitto.org", 1883, 60)
+client.loop_forever()
---
+++ iot-mqtt-bbb-sensor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import paho.mqtt.publish as mqtt
+import time
+import random
+
+# Dummy function to read from a temperature sensor.
+def readTemp():
+    return random.randint(80,100)
+
+# Deumm function to read from a rain sensor.
+def readPrecipitation():
+    r = random.randint(0,10)
+    if r < 4:
+        return 'rain'
+    elif r < 8:
+        return 'sun'
+    else:
+        return 'hurricane'
+    
+while True:
+    mqtt.single("iot-bbb-example/weather/temperature", readTemp(), hostname="test.mosquitto.org")
+    mqtt.single("iot-bbb-example/weather/precipitation", readPrecipitation(), hostname="test.mosquitto.org")
+    time.sleep(10)
